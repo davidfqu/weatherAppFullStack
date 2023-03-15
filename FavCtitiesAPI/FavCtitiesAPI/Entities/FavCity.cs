2f3b57fee30ed17b1eb83556c36a1833d682cb67
David Quiñonez <dquinonez@masimo.com>
2023-03-14 18:08:14 -0700

    full stack project

---
+++ FavCtitiesAPI/FavCtitiesAPI/Entities/FavCity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FavCtitiesAPI.Entities
+{
+    public class FavCity
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The city name is requried")]
+        [StringLength(maximumLength:50,ErrorMessage ="City name cannot have more than {0} characters")]
+        public string Name { get; set; }
+    }
+}
